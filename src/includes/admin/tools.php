91112402254263961130bca8b8969826bc1538c3
Brandon Allen <cnorris23@gmail.com>
2014-01-12 02:27:21 -0600

    First pass at tools.php

--- src/includes/admin/tools.php
+++ src/includes/admin/tools.php
@@ -1125,12 +1125,32 @@ function bbp_admin_repair_freshness() {
 			ON `topic`.`ID` = `reply`.`post_id` AND `reply`.`meta_key` = '_bbp_last_active_time'
 			WHERE `reply`.`meta_id` IS NULL AND `topic`.`post_type` = '{$tpt}' );" ) ) ) {
 		return array( 9, sprintf( $statement, $result ) );
-	}
+
+	// Give topics their last edit time
+	/*
+	if ( is_wp_error( $wpdb->query( "INSERT INTO `$wpdb->postmeta` (`post_id`, `meta_key`, `meta_value`)
+			( SELECT `topic`.`ID`, '_bbp_last_edit_time_gmt', `topic`.`post_modified_gmt`
+			FROM `$wpdb->posts` AS `topic`
+			WHERE `topic`.`post_type` = '" . bbp_get_topic_post_type() . "'
+			GROUP BY `topic`.`ID` );" ) ) )
+		return array( 10, sprintf( $statement, $result ) );
+	*/
+
+	// Also, update the post_modified and post_modified_gmt times
+	if ( is_wp_error( $wpdb->query( "UPDATE `$wpdb->posts`,
+			( SELECT `topic`.`ID` AS `ID`, MAX( `reply`.`post_date` ) AS `post_date`, MAX( `reply`.`post_date_gmt` ) AS `post_date_gmt`
+			FROM `$wpdb->posts` AS `topic` INNER JOIN `$wpdb->posts` AS `reply`
+			ON `topic`.`ID` = `reply`.`post_parent`
+			WHERE `reply`.`post_status` = '{$pps}' AND `topic`.`post_type` = '{$tpt}' AND `reply`.`post_type` = '{$rpt}'
+			GROUP BY `topic`.`ID` ) AS `update`
+			SET `$wpdb->posts`.`post_modified` = `update`.`post_date`, `$wpdb->posts`.`post_modified_gmt` = `update`.`post_date_gmt`
+			WHERE `$wpdb->posts`.`ID` = `update`.`ID`;" ) ) )
+		return array( 10, sprintf( $statement, $result ) );
 
 	// Forums need to know what their last active item is as well. Now it gets a bit more complex to do in the database.
 	$forums = $wpdb->get_col( "SELECT `ID` FROM `$wpdb->posts` WHERE `post_type` = '{$fpt}' and `post_status` != 'auto-draft';" );
-	if ( is_wp_error( $forums ) ) {
-		return array( 10, sprintf( $statement, $result ) );
+	if ( is_wp_error( $forums ) )
+		return array( 11, sprintf( $statement, $result ) );
 	}
 
  	// Loop through forums
